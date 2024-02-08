bb0f42166a33d1fff31236c9eb6cdfcc7ec60238
eliott-esiee <eliott.vigier@edu.esiee.fr>
2024-02-08 20:29:45 +0100

    oue

--- es-init/transfer_script.py
+++ es-init/transfer_script.py
@@ -6,31 +6,44 @@ import json
 # Connexion à MongoDB
 mongo_client = MongoClient('mongodb://mongo:27017/')
 mongo_db = mongo_client['iphone_db']
-mongo_collection = mongo_db['models']  # Remplacez par le nom de votre collection
+mongo_collection = mongo_db['models']
 
 # Connexion à Elasticsearch
-es = Elasticsearch('http://elasticsearch:9200')  # Assurez-vous que l'adresse correspond à votre configuration
+es = Elasticsearch('http://elasticsearch:9200')
 
-# Création d'un index Elasticsearch si nécessaire
+# Assurez-vous que l'adresse correspond à votre configuration
 if not es.indices.exists(index="iphone_index"):
     es.indices.create(index="iphone_index")
 
-# Fonction pour convertir de MongoDB vers un format compatible JSON pour Elasticsearch
+
 def mongo_doc_to_es(doc):
-    # Utilise json_util pour convertir le document MongoDB en JSON
-    doc_json = json.loads(json_util.dumps(doc))
-    # Supprimez les clés non nécessaires ou ajustez le document ici si nécessaire
-    doc_json.pop('_id', None)  # Supprime l'_id MongoDB s'il n'est pas nécessaire dans Elasticsearch
-    return doc_json
+    # Transforme le document MongoDB en JSON
+    return json.loads(json_util.dumps(doc))
 
-# Récupération des documents de MongoDB et indexation dans Elasticsearch
-docs = mongo_collection.find()
-actions = [
-    {
-        "_index": "iphone_index",
-        "_source": mongo_doc_to_es(doc)
-    }
-    for doc in docs
-]
+def generate_es_documents(docs):
+    for doc in docs:
+        gamme = doc['gamme']
+        for modele in doc['modèles']:
+            nom_modele = modele['nom']
+            for variante in modele['variantes']:
+                stockage = variante['stockage']
+                for couleur in variante['couleurs']:
+                    # Créer un document pour chaque variante de couleur
+                    doc_es = {
+                        'gamme': gamme,
+                        'nom': nom_modele,
+                        'stockage': stockage,
+                        'couleur': couleur['couleur'],
+                        'prix': couleur['prix'],
+                        'date': couleur['date']
+                    }
+                    yield {
+                        "_index": "iphone_index",
+                        "_source": doc_es
+                    }
+                    
 
+# Indexation des documents dans Elasticsearch
+docs_mongo = mongo_collection.find()
+actions = list(generate_es_documents(docs_mongo))
 helpers.bulk(es, actions)
