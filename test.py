0b0901ed51c445550bf0735ace6703324523ae2b
eliott-esiee <eliott.vigier@edu.esiee.fr>
2024-02-08 22:09:38 +0100

    ca marche

--- test.py
+++ test.py
@@ -4,28 +4,40 @@ from elasticsearch import Elasticsearch
 app = Flask(__name__)
 es = Elasticsearch('http://localhost:9200')
 
-# Page d'accueil avec la barre de recherche
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        query = request.form['search']
-        results = search_in_elasticsearch(query)
+        search_name = request.form.get('search_name', '')
+        search_color = request.form.get('search_color', '')
+        search_storage = request.form.get('search_storage', '')
+        results = search_in_elasticsearch(search_name, search_color, search_storage)
         return render_template_string(SEARCH_RESULTS_TEMPLATE, results=results)
     return render_template_string(HOME_TEMPLATE)
 
-def search_in_elasticsearch(query):
+def search_in_elasticsearch(search_name, search_color, search_storage):
+    query_filters = []
+    
+    if search_name:
+        query_filters.append({"match": {"nom": search_name}})
+    if search_color:
+        query_filters.append({"match": {"couleur": search_color}})
+    if search_storage:
+        query_filters.append({"match": {"stockage": search_storage}})
+    
     body = {
         "query": {
-            "multi_match": {
-                "query": query,
-                "fields": ["nom", "modèles.nom", "modèles.variantes.stockage", "modèles.variantes.couleurs.couleur", "modèles.variantes.couleurs.prix"]
+            "bool": {
+                "must": query_filters
             }
         },
         "size": 10
     }
+
     response = es.search(index="iphone_index", body=body)
     return [hit["_source"] for hit in response['hits']['hits']]
 
+# Le template de la page d'accueil doit être mis à jour pour inclure les champs de recherche supplémentaires
 HOME_TEMPLATE = '''
 <!doctype html>
 <html>
@@ -33,12 +45,15 @@ HOME_TEMPLATE = '''
 <body>
   <h2>Recherche iPhone</h2>
   <form method="post">
-    <input type="text" name="search" />
+    <input type="text" name="search_name" placeholder="Nom" />
+    <input type="text" name="search_color" placeholder="Couleur" />
+    <input type="text" name="search_storage" placeholder="Stockage" />
     <input type="submit" value="Chercher" />
   </form>
 </body>
 </html>
 '''
+
 
 SEARCH_RESULTS_TEMPLATE = '''
 <!doctype html>
@@ -48,7 +63,7 @@ SEARCH_RESULTS_TEMPLATE = '''
   <h2>Résultats de la recherche</h2>
   <ul>
     {% for result in results %}
-      <li>{{ result }}</li>
+      <li>Gamme: {{ result['gamme'] }}, Nom: {{ result['nom'] }}, Stockage: {{ result['stockage'] }}, Couleur: {{ result['couleur'] }}, Prix: {{ result['prix'] }}, Date: {{ result['date'] }}</li>
     {% endfor %}
   </ul>
   <a href="/">Nouvelle recherche</a>
