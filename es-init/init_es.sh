6a1f0017b962be6b7e27dac1f239412a9ed71cc2
“Joshua <joshua.dumont@edu.esiee.fr>
2024-02-08 15:44:17 +0100

    modification elastic searsh (pas stable)

--- es-init/init_es.sh
+++ es-init/init_es.sh
@@ -3,10 +3,10 @@
 # Attendre qu'Elasticsearch soit disponible
 while ! curl -s http://elasticsearch:9200/_cluster/health > /dev/null; do
   echo "En attente d'Elasticsearch..."
-  sleep 5
+  sleep 1
 done
 
 echo "Elasticsearch est prêt."
 
 # Exécuter le script de transfert de données
-python /transfer_script.py
+python /app/transfer_script.py
